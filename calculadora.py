c7e2e49b89f73d652424b204173f29458768eb5c
rafaela28costa <a14639@esars.pt>
2024-11-05 09:43:03 +0000

    funcoes para a calc

---
+++ calculadora.py
@@ -0,0 +1,18 @@
+# Função para soma
+def soma(x, y):
+    return x + y
+
+
+#Função para multiplicação
+def multiplicação(x, y):
+    return x * y
+
+
+#Função para divisão
+def divisão(x, y):
+    return x / y
+
+
+#Função pra subtração
+def subtração(x, y):
+    return x - y
